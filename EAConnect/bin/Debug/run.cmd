8883e818b9b8c1d98bd8bd48e1602ba82ec0327b
Tivil <i9150713593@gmail.com>
2015-10-27 12:44:17 +0300

    delete

--- EAConnect/bin/Debug/run.cmd
+++
@@ -1,4 +0,0 @@
-EAReport Каскад.eap "Поведенческая модель(expert).docx" "ПК \"Каскад\"~Клиент~Библиотека Expert~Поведенческая модель" sequential "C:\\Cascade\\trunk\\doc\\le.dotx"
-echo %errorlevel%
-
-
