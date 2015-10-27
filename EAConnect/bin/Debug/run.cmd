ada9de277f23954fc9ca1a9380956e85aebc06cc
Tivil <i9150713593@gmail.com>
2015-10-27 12:41:18 +0300

    add visual studio project

---
+++ EAConnect/bin/Debug/run.cmd
@@ -0,0 +1,4 @@
+EAReport Каскад.eap "Поведенческая модель(expert).docx" "ПК \"Каскад\"~Клиент~Библиотека Expert~Поведенческая модель" sequential "C:\\Cascade\\trunk\\doc\\le.dotx"
+echo %errorlevel%
+
+
